f4f5e7b30adee8b420dd3bcad1e76ed08f788390
Moonbius-lab <764371130@qq.com>
2026-08-04 00:51:16 +0800

    chore: 移除两张壁纸

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -43,10 +43,8 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/wallpaper-1.avif",
 			"assets/images/DesktopWallpaper/wallpaper-2.avif",
 			"assets/images/DesktopWallpaper/wallpaper-3.avif",
-			"assets/images/DesktopWallpaper/wallpaper-4.avif",
 			"assets/images/DesktopWallpaper/wallpaper-5.avif",
 			"assets/images/DesktopWallpaper/wallpaper-6.avif",
 		],
